34b36c81a6ce469fb2497b1de8d4df7a0f3ec307
Chulpan Valiullina <c.valiullina@innopolis.university>
2025-04-24 14:46:04 +0300

    connecting to back without tests

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
-export const BACKEND_URL = 'http://localhost:5000/messages';
-export const SEND_TOPIC_URL = 'http://localhost:5000/send-topic';
-export const SIGNAL_URL = 'http://localhost:5000/signal';
-export const FETCH_INTERVAL = 3000; // 3 seconds
+const BASE_URL = import.meta.env.DEV ? '/api' : 'http://localhost:5000';
 
+export const BACKEND_URL = `${BASE_URL}/messages`;
+export const SEND_TOPIC_URL = `${BASE_URL}/send-topic`;
+export const SIGNAL_URL = `${BASE_URL}/signal`;
+export const FETCH_INTERVAL = 3000; // 3 seconds
